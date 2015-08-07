56de28c336bc94dbd1e6f906f95b60a9261c0066
Ibrahim Zahoor <ibrahimzahoor.nu@gmail.com>
2015-08-07 11:17:05 +0500

    Removed insecure and auto publish

---
+++ client/templates/posts/post-submit.js
@@ -0,0 +1,15 @@
+Template.postSubmit.helpers({
+
+});
+
+Template.postSubmit.events({
+
+  "submit form": function (event, template) {
+    Posts.insert({
+      title: event.target.title.value,
+      url: event.target.url.value,
+      description: event.target.description.value
+    });
+  }
+
+});
